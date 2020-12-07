16f1d13701e12e208ffeefc1ce298beee5111ac3
ThakshilaNanayakkara <74827740+ThakshilaNanayakkara@users.noreply.github.com>
2020-12-07 22:14:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$email = $_POST['email'];
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$to = "thakshilannanayakkara@gmail.com";
+$body = "";
+
+$body .= "Email: ".$email. "\r\n";
+$body .= "From: ".$name. "\r\n";
+$body .= "Message: ".$message. "\r\n";
+
+mail($to, $subject, $body);
+
+
+?>
+
